b5ced8d60c0206bf076f732c6a909a70ad4f48ad
Sebastian Bermudez Herrera <71651833+sebasbermuher@users.noreply.github.com>
2021-11-30 18:59:14 +0100

    Delete Unidad 3 directory

--- Unidad 3/Tarea1-Date/Ejercicio1/script.js
+++
@@ -1,9 +0,0 @@
-let fecha = new Date();
-
-let fechaFinCurso = new Date("Jun 24 2022");
-
-let resta = fechaFinCurso - fecha;
-
-let diasRestantes = Math.floor(resta / (1000 * 60 * 60 * 24)); // (resta/milisegundos de un dia)
-
-alert("Quedan " + diasRestantes + " dias para terminar el curso");
